6c4b964133f282f6262943c8aec05f54af51ed97
Schouketta <sarah.le@live.fr>
2020-07-17 16:02:27 +0200

    nouveau card slider + beau

--- frontend/src/Components/CardSlider.js
+++ frontend/src/Components/CardSlider.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import CardVanne from './CardVanne';
-import Carousel from 'react-elastic-carousel';
+/*import Carousel from 'react-elastic-carousel';*/
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import "./MainContent.css";
 
 
@@ -16,21 +19,52 @@ class CardSlider extends Component {
           {id: 5, title: 'item #5'}
         ]
       }
-    
+
     render() {
 
         const { items } = this.state;
 
+        var settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        };
+
         return (
-            <Carousel  itemsToShow={3} 
-            itemsToScroll={1} 
-            itemPadding={[10, 30]}
-            
-            >
-                {items.map(item => <div key={item.id}><CardVanne></CardVanne></div>)}
-            </Carousel>
+          <Slider className="sliderContainer" {...settings}>
+            {items.map(item => <div className="slider" key={item.id}><CardVanne></CardVanne></div>)}
+          </Slider>
         );
-    }
+      }
+
 }
 
 export default CardSlider;
