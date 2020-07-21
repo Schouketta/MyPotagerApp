1b5e17e356b44ba4b5ffb0f1368994ad5781686c
Schouketta <sarah.le@live.fr>
2020-07-21 12:53:18 +0200

    Card slider avec data

--- frontend/src/Components/CardSlider.js
+++ frontend/src/Components/CardSlider.js
@@ -5,12 +5,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./MainContent.css";
+import { Card, Button, CardHeader, CardBody,
+  CardText, CustomInput} from 'reactstrap';
 
 
 
 class CardSlider extends Component {
     
-    state = {
+    /*state = {
         items: [
           {id: 1, title: 'item #1'},
           {id: 2, title: 'item #2'},
@@ -18,11 +20,29 @@ class CardSlider extends Component {
           {id: 4, title: 'item #4'},
           {id: 5, title: 'item #5'}
         ]
-      }
+      }*/
+
+    constructor(props) 
+    {
+        super(props);
+    
+        this.state = {
+            activeTab : '1',
+            tableauVannes : [
+              {id:1, nomVanne:'Vanne Tomates',            adresseIP:'198.178.22', statut:'ON',   type:'fruits'},
+              {id:2, nomVanne:'Vanne Framboises/Mures',   adresseIP:'198.178.21', statut:'OFF',  type:'légumes' },
+              {id:3, nomVanne:'Vanne Herbes Aromatiques', adresseIP:'197.178.23', statut:'OFF',  type:'herbes aromatiques'},
+              {id:4, nomVanne:'Vanne Radis',              adresseIP:'198.178.26', statut:'OFF',  type:'légumes'},
+              {id:5, nomVanne:'Vanne Courgettes',         adresseIP:'198.178.28', statut:'ON',   type:'légumes'}
+
+            ],
+          };
+    
+    }
 
     render() {
 
-        const { items } = this.state;
+        //const { items } = this.state;
 
         var settings = {
           dots: true,
@@ -60,8 +80,22 @@ class CardSlider extends Component {
 
         return (
           <Slider className="sliderContainer" {...settings}>
-            {items.map(item => <div className="slider" key={item.id}><CardVanne></CardVanne></div>)}
+            {this.state.tableauVannes.map(vanne => 
+              <div className="slider" key={vanne.id}>
+                <Card> 
+                  <CardHeader>{vanne.nomVanne}</CardHeader>
+                  <CardBody>
+                    <CardText>Statut : {vanne.statut}</CardText>
+                    <CardText>IP : {vanne.adresseIP}</CardText>
+                    <CustomInput type="switch" id={"switch"+vanne.id} label="Turn on" />
+                    <Button className="buttonCardVanne">Attribuer un programme</Button>
+                  </CardBody>
+                </Card>
+                </div>)
+            }
           </Slider>
+          //{items.map(item => <div className="slider" key={item.id}><CardVanne></CardVanne></div>)}
+
         );
       }
 
--- frontend/src/Components/CardVanne.js
+++ frontend/src/Components/CardVanne.js
@@ -5,17 +5,35 @@ import "./CardVanne.css";
 
 
 class CardVanne extends Component {
+
+    constructor(props) 
+    {
+        super(props);
+    
+        this.state = {
+            activeTab : '1',
+            tableauVannes : [
+              {id:1, nomVanne:'Vanne Tomates',            adresseIP:'198.178.22', statut:'ON',   type:'fruits'},
+              {id:2, nomVanne:'Vanne Framboises/Mures',   adresseIP:'198.178.21', statut:'OFF',  type:'légumes' },
+              {id:3, nomVanne:'Vanne Herbes Aromatiques', adresseIP:'197.178.23', statut:'OFF',  type:'herbes aromatiques'},
+              {id:4, nomVanne:'Vanne Radis',              adresseIP:'198.178.26', statut:'OFF',  type:'légumes'}
+            ],
+          };
+    
+    }
+
     render() {
         return (
-                <Card>
-                    <CardHeader>NomVanne</CardHeader>
+              /* <Card> 
+                    <CardHeader>{nomVanne}</CardHeader>
                     <CardBody>
-                    <CardText>Statut : </CardText>
-                    <CardText>IP : </CardText>
+                    <CardText>Statut : {statut}</CardText>
+                    <CardText>IP : {adresseIP}</CardText>
                     <CustomInput type="switch" id="exampleCustomSwitch" name="customSwitch" label="Turn on arrosage" />
                     <Button className="buttonCardVanne">Attribuer un programme</Button>
                     </CardBody>
-                </Card>
+                </Card>*/ 
+                <div></div>
         );
     }
 }
